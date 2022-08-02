6b395e1168480fd4893879b253b90062bebac86c
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-08-02 14:09:20 +0300

    add controllers for all modules

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,4 +1,5 @@
 export class ProductModel {
+  _id: string;
   image: string;
   title: string;
   price: number;
@@ -14,4 +15,3 @@ export class ProductModel {
     [key: string]: string;
   };
 }
- 
