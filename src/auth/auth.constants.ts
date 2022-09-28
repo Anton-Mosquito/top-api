0ddb02903bb9214dfd32ec43c978fe7724b1548b
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-09-28 15:57:48 +0300

    add jwt module for authorization,  add constants for exceptions,  add validate user

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
 export const ALREADY_REGISTERED_ERROR = 'User with this email have already registered';
+export const USER_NOT_FOUND_ERROR = 'User with this name can not be found';
+export const WRONG_PASSWORD_ERROR = 'Wrong password'
