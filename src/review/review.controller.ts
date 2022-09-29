e3310b3be7f166c0c7058797abbd7b420f46685d
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-09-29 16:34:49 +0300

    add passport, jwt passport package, create jwt guard, jwt strategy add guard to review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { JwtAuthGard } from 'src/auth/guards/jwt.guard';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
@@ -14,6 +15,7 @@ export class ReviewController {
     return this.reviewService.create(dto);
   } 
 
+  @UseGuards(JwtAuthGard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
@@ -22,6 +24,7 @@ export class ReviewController {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
     }
   }
+
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
